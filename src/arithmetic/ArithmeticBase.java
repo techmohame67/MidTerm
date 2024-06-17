958b206a5907a831b40a8035c2b8d8094f44741c
techmohame67 <mohame67@sheridancollege.ca>
2024-06-17 18:16:49 -0400

    mid term operation class added

--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * from user and execute the arithmetic operation
  * change the code to use enum instead String and mention the advantage of enum.
  * @author sivagamasrinivasan
- * @modifier MohamedIdris, 991716127
+ *  * @modifier MohamedIdris, 991716127
  * 
  */
 public class ArithmeticBase 
